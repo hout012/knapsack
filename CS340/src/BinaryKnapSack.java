802da4293063919be7bb51275bc95069ad2773eb
a1 <a1@192.168.1.111>
2019-06-08 14:22:22 +0700

    First commit

---
+++ CS340/src/BinaryKnapSack.java
@@ -0,0 +1,83 @@
+import java.awt.Dimension;
+import java.util.ArrayList;
+import java.util.List;
+//HELLO WORLD
+public class BinaryKnapSack {
+	private int maxWeight = 0;
+	ArrayList<Integer> result = new ArrayList<Integer>();
+	ArrayList<Integer> dumpItem = new ArrayList<Integer>();
+	ArrayList<Integer> wt = new ArrayList<Integer>();
+	ArrayList<Integer> val = new ArrayList<Integer>();
+	ArrayList<String> name = new ArrayList<String>();
+	public BinaryKnapSack() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public void createItem(String name,int weight,int price) {
+		this.wt.add(weight);
+		this.name.add(name);
+		this.val.add(price);
+		
+	}
+	
+	void knapSackDp(int W, int n) {
+		int K[][] = new int[n + 1][W + 1]; 
+		int i,w;
+		// Build DP Table K[i][w]
+        for (i = 0; i <= n; i++) { 
+            for (w = 0; w <= W; w++) { 
+                if (i == 0 || w == 0) 
+                    K[i][w] = 0; 
+                else if (wt.get(i - 1) <= w) 
+                    K[i][w] = Math.max(val.get(i - 1) +  
+                              K[i - 1][w - wt.get(i - 1)], K[i - 1][w]); 
+                else
+                    K[i][w] = K[i - 1][w]; 
+            } 
+        } 
+        int res =  K[n][W];
+        System.out.println(res);
+        //Start Backtracking
+        w = W; 
+        for (i = n; i > 0 && res > 0; i--) { 
+            if (res == K[i - 1][w]) {
+                continue; // we didn't picked the items
+            }
+            else {
+            	//Items get picked
+                System.out.println("Item name: "+name.get(i-1)+" Price: "+wt.get(i-1) + " "); 
+                result.add(i-1);
+                res = res - val.get(i - 1); 
+                w = w - wt.get(i - 1); 
+            }
+        }  
+	}
+
+	
+	void setMaxWeight(int w) {
+		this.maxWeight = w;
+	}
+	
+	int getMaxWeight() {
+		return this.maxWeight;
+	}
+	
+	ArrayList<Integer> getWeightList(){
+		return this.wt;
+	}
+	
+	ArrayList<Integer> getPriceList(){
+		return this.val;
+	}
+	ArrayList<String> getNameList(){
+		return this.name;
+	}
+	
+	ArrayList<Integer> getResult(){
+		return this.result;
+	}
+	
+	void BranchAndBound() {
+		
+	}
+}
---
+++ CS340/src/GraphicUI.java
@@ -0,0 +1,108 @@
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+
+import javax.swing.*;
+public class GraphicUI {
+	int W = 0;
+	int n =0;
+	public GraphicUI() {
+		
+		BinaryKnapSack test = new BinaryKnapSack();
+		JFrame win = new JFrame();
+		//JList list = new JList(test.getItems().toArray());
+		JTabbedPane t1 = new JTabbedPane();
+		JPanel p1 = new JPanel();
+		JPanel p2 = new JPanel();
+		p2.setBackground(Color.black);
+		JMenuBar bar = new JMenuBar();
+		JMenu menu = new JMenu("File");
+		JMenuItem imp = new JMenuItem("Import");
+		JMenuItem exp = new JMenuItem("Export");
+		menu.add(imp);menu.add(exp);bar.add(menu);
+		win.setJMenuBar(bar);
+		JButton add = new JButton("Add Item");
+		JLabel maxWeight = new JLabel("Maximum Weight");
+		JLabel knap = new JLabel("Knapsack solution");
+		//test
+		JLabel n1 = new JLabel("Solutions: MaxProfit = 235");
+
+		//test
+		p2.add(n1);
+		JTextField w = new JTextField(5); 
+		w.setText("0");
+		JButton dks = new JButton("Start");
+		win.setLayout(new BoxLayout(win.getContentPane(),BoxLayout.X_AXIS));
+		p2.setLayout(new BoxLayout(p2,BoxLayout.Y_AXIS));
+		p2.setPreferredSize(new Dimension(400,100));
+		p2.setBackground(Color.white);
+		t1.setPreferredSize(new Dimension(200,100));
+		p1.add(add);p1.add(maxWeight);p1.add(w);p1.add(knap);p1.add(dks);
+		t1.add(p1,"Items");
+		win.add(p2);win.add(t1);
+		win.setBounds(50, 50, 600, 450);
+		win.setVisible(true);
+		win.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+		
+		
+		add.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+//				  JTextField name = new JTextField(10);
+//				  JTextField weight = new JTextField(5);
+//			      JTextField price =  new JTextField(5);
+//			      JPanel myPanel = new JPanel();
+//			      myPanel.setLayout(new BoxLayout(myPanel,BoxLayout.PAGE_AXIS));
+//			      myPanel.add(new JLabel("Name"));
+//			      myPanel.add(name);
+//			      myPanel.add(new JLabel("Weight"));
+//			      myPanel.add(weight);
+//			      myPanel.add(new JLabel("Price"));
+//			      myPanel.add(price);
+//			      
+//			      int result = JOptionPane.showConfirmDialog(null, myPanel, "Please Enter Item infos", JOptionPane.OK_CANCEL_OPTION);
+//			      if(result == 0 ) {
+//			    	  n++;
+//			    	  test.createItem(name.getText(), Integer.parseInt(weight.getText()),  Integer.parseInt(price.getText()));
+//			    	  String str = "Name: "+name.getText()+", Price ($): "+price.getText()+", Weight (kg): "+weight.getText();
+//			    	  JLabel n = new JLabel(str);
+//			    	  p2.add(n);
+//			    	  win.revalidate();
+//			    	
+//			      }
+				
+				
+				
+			}
+		});
+		
+		dks.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				W = Integer.parseInt(w.getText());
+				test.knapSackDp(W,n);
+				
+				p2.removeAll();
+				for (Integer num : test.getResult()) { 		      
+					String str = "Name: "+test.getNameList().get(num)+", Price ($): "+
+							test.getPriceList().get(num)+", Weight (kg): "+test.getWeightList().get(num);
+					JLabel n = new JLabel(str);
+					n.setForeground(Color.green);
+					p2.add(n);		
+			      }
+				p2.revalidate();
+				
+				
+			}
+		});
+	}
+	public static void main(String args[]) {
+		GraphicUI test = new GraphicUI();
+	}
+	
+	
+
+}
